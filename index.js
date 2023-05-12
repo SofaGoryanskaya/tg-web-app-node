966dc712ee4fdab3f44f460de7f07f4400244948
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-12 22:49:41 +0700

    init

---
+++ index.js
@@ -0,0 +1,86 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const express = require('express');
+const cors = require('cors');
+
+const token = '6132392940:AAG3R5s1IK6HfOgBk2FzWNbWttXyQnWzBq0';
+const WebAppUrl = 'https://tg-bot-d412c.web.app';
+
+const bot = new TelegramBot(token, {polling: true});
+app.use(express.json());
+app.use(cors());
+
+//подсказки /...
+bot.setMyCommands([
+    {command: '/start', description: 'Hello'},
+    {command: '/form', description: 'Form'}
+
+])
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        console.log(msg.text);
+        bot.sendMessage(chatId, "Здравствуйте!\n" +
+            "Мы ради приветствовать Вас в нашем новом боте!", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Меню', web_app: {url: WebAppUrl + '/menu'}}],
+                    [{text: 'Профиль', web_app: {url: WebAppUrl + '/prof'}}],
+                    [{text: 'Корзина', web_app: {url: WebAppUrl}}]
+                ]
+            }
+        })
+    }
+    if(text === '/form') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+        reply_markup: {
+            keyboard: [
+                [{text: 'Заполнить форму', web_app: {url: WebAppUrl + '/form'}}]
+            ]
+        }
+    })
+}
+
+
+    if(msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
+    }
+});
+
+
+// app.post('/web-data', async (req, res) => {
+//     const {queryId, products = [], totalPrice} = req.body;
+//     try {
+//         await bot.answerWebAppQuery(queryId, {
+//             type: 'article',
+//             id: queryId,
+//             title: 'Успешная покупка',
+//             input_message_content: {
+//                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+//             }
+//         })
+//         return res.status(200).json({});
+//     } catch (e) {
+//         return res.status(500).json({})
+//     }
+// })
+//
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
+
