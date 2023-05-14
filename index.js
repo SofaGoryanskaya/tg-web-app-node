c6117d49f17418010a49eb78e8080cafd5516aa6
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-15 03:19:28 +0700

    change form fetch

--- index.js
+++ index.js
@@ -65,15 +65,32 @@ bot.on('message', async (msg) => {
 });
 
 
+// app.post('/web-data', async (req, res) => {
+//     const {queryId, products = [], totalPrice} = req.body;
+//     try {
+//         await bot.answerWebAppQuery(queryId, {
+//             type: 'article',
+//             id: queryId,
+//             title: 'Успешная покупка',
+//             input_message_content: {
+//                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+//             }
+//         })
+//         return res.status(200).json({});
+//     } catch (e) {
+//         return res.status(500).json({})
+//     }
+// })
+
 app.post('/web-data', async (req, res) => {
-    const {queryId, products = [], totalPrice} = req.body;
+    const {queryId, country} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${country}`
             }
         })
         return res.status(200).json({});
