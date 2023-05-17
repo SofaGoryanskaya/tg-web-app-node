7aa89762d3b776d2cd1a56e7bebbc16941a28f00
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-17 13:13:27 +0700

    change form -10

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ bot.on('message', async (msg) => {
 
                     [{text: 'Профиль', web_app: {url: WebAppUrl + '/prof'}}],
                     [{text: 'Корзина', web_app: {url: WebAppUrl}}],
-                    [{text: 'Меню', web_app: {url: WebAppUrl + '/menu'}}]
+                    [{text: 'Меню', web_app: {url: WebAppUrl + '/menu'}}],
+                    [{text: 'Заполнить форму', web_app: {url: WebAppUrl + '/form'}}]
 
                 ]
             }
