58074815674f4d5b8040147cd7d70b4a454c4eda
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-17 13:21:58 +0700

    change form -10

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
         reply_markup: {
             keyboard: [
-                [{text: 'Заполнить форму', web_app: {url: WebAppUrl + '/form'}}]
+                [{text: 'Заполнить форму', web_app: {url: WebAppUrl + '/form'}}],
+                [{text: 'Меню', web_app: {url: WebAppUrl + '/menu'}}],
 
             ]
         }
