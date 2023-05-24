bd2cc73253d447a36b82ad0acf2f8a79fcaf47c1
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-24 16:22:28 +0700

    msg /

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ bot.on('message', async (msg) => {
                 ]
             }
         })
-    } else if (text === '/.'){
+    } else if (text.indexOf('/') !== -1){
         await bot.sendMessage(chatId, 'Визитка нашей кофейни' + '\n' +
             "/start - Приветсвтие \n" +
             "/menu - Главное меню\n" +
